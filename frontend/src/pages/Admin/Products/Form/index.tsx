b5f98ebb395666ea39bf25a16b3481a1ab769374
auriceliof <auricelio.suporte@hotmail.com>
2022-06-22 20:23:52 -0300

    Select React Hook Form integration

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
 import { Category } from 'types/category';
@@ -27,6 +27,7 @@ const Form = () => {
     handleSubmit,
     formState: { errors },
     setValue,
+    control,
   } = useForm<Product>();
 
   useEffect(() => {
@@ -98,16 +99,28 @@ const Form = () => {
                 <div className="invalid-feedback d-block">
                   {errors.name?.message}
                 </div>
-              </div>
-
+              </div> 
+              
               <div className="margin-bottom-30">
-                <Select 
-                  options={selectCategories}
-                  classNamePrefix="produc-crud-select"
-                  isMulti
-                  getOptionLabel={(category: Category) => category.name}
-                  getOptionValue={(category: Category) => String(category.id)}
+                <Controller 
+                  name="categories"
+                  rules={{ required: true}}
+                  control={control}
+                  render={({ field }) => (
+                    <Select {...field}
+                      options={selectCategories}
+                      classNamePrefix="produc-crud-select"
+                      isMulti
+                      getOptionLabel={(category: Category) => category.name}
+                      getOptionValue={(category: Category) => String(category.id)}
+                    />
+                  )}
                 />
+                {errors.categories && (
+                   <div className="invalid-feedback d-block">
+                   Campo obrigatório
+                 </div>
+                )}
               </div>
 
               <div className="margin-bottom-30">
