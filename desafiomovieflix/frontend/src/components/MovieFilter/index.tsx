2e7f666c61d67aa88a0d3b20de0c473e8f5a4973
auriceliof <auricelio.suporte@hotmail.com>
2022-07-07 21:46:58 -0300

    React select layout

--- desafiomovieflix/frontend/src/components/MovieFilter/index.tsx
+++ desafiomovieflix/frontend/src/components/MovieFilter/index.tsx
@@ -1,13 +1,21 @@
+import Select from 'react-select';
 import './styles.css';
 
 const MovieFilter = () => {
+  const options = [
+    { value: 'Aventura', label: 'Aventura' },
+    { value: 'Ação', label: 'Ação' },
+    { value: 'Terror', label: 'Terror' },
+  ];
+
   return (
     <div className="base-card moviefilter-container">
       <form action="" className="moviefilter-form">
-        <div className="moviefilter-category">
-          <select className="moviefilter-category-select" name="" id="">
-            <option value="">Aventura</option>
-          </select>
+        <div className="moviefilter-category-">
+          <Select 
+            options={options} 
+            classNamePrefix="moviefilter-select"
+          />
         </div>
       </form>
     </div>
